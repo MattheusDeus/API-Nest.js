0fd5a1b8faf8aa6338cead6d7ccb18ae38b57058
Matheus Deus <matheusmeris05@outlook.com>
2025-03-25 16:33:23 -0300

    MODIFICANDO

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -6,12 +6,21 @@ export class TasksController {
   constructor(private readonly tasksService: TasksService) { }
 
   @Get()
-  getTasks() {
-    return this.tasksService.listAllTasks();
+  findAllTasks() {
+    return this.tasksService.findAll;
   }
-  @Get('/teste')
-  getTeste() {
-    return 'Teste das Tasks';
+  @Get(":id")
+  findOneTask(@Param('id') id: string) {
+    return this.tasksService.findOne(id);
   }
+  @Post()
+  createTask(@Body() body: any) {
+    console.log(body);
+    return this.tasksService.create(body);
 
+  }
 }
+
+
+
+
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -3,11 +3,21 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class TasksService {
 
-  listAllTasks() {
+  findAll() {
     return [
       { id: 1, title: 'Comprar Pão ', description: 'Pão doce' }
     ];
 
   }
+
+  findOne(id: string) {
+    return "Buscar tarefa com id " + id;
+  }
+
+  create(body: any) {
+    console.log(body);
+    return "Criar tarefa com os dados " + body;
+  }
+
 
 }
